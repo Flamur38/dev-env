470ba0c2ae8deeacce28998fe699cd80087b4ae9
Flamur38 <mulihtb@proton.me>
2025-04-08 23:01:22 +0200

    added rose-pine to i3

--- config-files/.config/polybar/launch_polybar.sh
+++ config-files/.config/polybar/launch_polybar.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
 
-# Kill already running polybar instances
+# Kill already running bars
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u "$UID" -x polybar >/dev/null; do
-    sleep 1
-done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar on all connected monitors
-if type "xrandr" >/dev/null 2>&1; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload toph &
-    done
-else
-    # No xrandr found, just launch normally
-    polybar --reload toph &
-fi
+# Launch Polybar
+MONITOR=$(xrandr --query | grep " connected" | cut -d" " -f1 | head -n1)
+polybar --reload toph &
 
+
+# #!/usr/bin/env bash
+#
+# # Kill already running polybar instances
+# killall -q polybar
+#
+# # Wait until the processes have been shut down
+# while pgrep -u "$UID" -x polybar >/dev/null; do
+#     sleep 1
+# done
+#
+# # Launch polybar on all connected monitors
+# if type "xrandr" >/dev/null 2>&1; then
+#     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#         MONITOR=$m polybar --reload toph &
+#     done
+# else
+#     # No xrandr found, just launch normally
+#     polybar --reload toph &
+# fi
